8092fc7da6fa3fecc8a08d158823f9fa458eb59c
Hoang Van Duc <duch52362@gmail.com>
2024-07-27 15:24:07 +0700

    #7 CRUD With Nodejs (Add User)

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from "../models/index";
+import CRUDService from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
   try {
@@ -13,7 +14,19 @@ let getAboutPage = (req, res) => {
   return res.render("test/about.ejs");
 };
 
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  return res.send("muon dit be van qua");
+};
+
 module.exports = {
   getHomePage,
   getAboutPage,
+  getCRUD,
+  postCRUD,
 };
